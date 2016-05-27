ece37166869dc9f685a0193051f75322e29ded6f
Rick Thomas <rick@rthomasmusic.com>
2016-05-27 02:21:25 -0400

    Removed extra brackets leftover from example Mongo code

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
 // API ROUTES BELOW
