9ca58f865d8eb88d02e24269b7cc64f7d1c4801c
Rick Thomas <rick@rthomasmusic.com>
2016-05-27 02:18:45 -0400

    Removed all contacts references and build API route stubs

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-var CONTACTS_COLLECTION = "contacts";
-
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -15,4 +13,33 @@ app.use(bodyParser.json());
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/comments"
+ *    GET: finds all comments
+ *    POST: creates a new contact
+ */
+
+app.get("/comments", function(req, res) {
+});
+
+app.post("/comments", function(req, res) {
+});
+
+/*  "/comments/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/comments/:id", function(req, res) {
+});
+
+app.put("/comments/:id", function(req, res) {
+});
